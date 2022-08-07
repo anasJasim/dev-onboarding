dc1ab3a305916f68a822c71a5ca18280d3531711
Anas <anas.jasim9911@gmail.com>
2022-08-07 17:32:34 +0300

    make test pass: LocalStoragePersistenceAdapter.get return null when item not exist

--- src/services/localStoragePersistenceAdapter.ts
+++ src/services/localStoragePersistenceAdapter.ts
@@ -2,8 +2,8 @@ export class LocalStoragePersistenceAdapter {
   async set(key: string, value: string) {
     localStorage.setItem(key, value);
   }
-  async get(key: string): Promise<string> {
-    return localStorage.getItem(key) || '';
+  async get(key: string): Promise<string | null> {
+    return localStorage.getItem(key);
   }
   async deleteAll() {
     localStorage.clear();
